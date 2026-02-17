166cef9d14f95aa5c35e57122e2ef403e14588f9
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2026-02-17 11:01:47 -0300

    refactor: reorganize imports and remove unused CSS parser configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme/theme-provider";
-import Header02 from "@/components/header/Header02";
 import { Toaster } from "sonner";
+import Header02 from "@/components/header/Header02";
+import { ThemeProvider } from "@/components/theme/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
